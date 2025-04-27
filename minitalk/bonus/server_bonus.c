8070cf159d41001c601fb02b46db21002f6b8114
Wkannouf <wkannouf@student.42.fr>
2025-04-27 06:31:31 +0100

    minitalk

---
+++ minitalk/bonus/server_bonus.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 23:45:50 by wkannouf          #+#    #+#             */
+/*   Updated: 2025/03/11 00:32:20 by wkannouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+
+static int	g_pid_c;
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + 48);
+}
+
+static void	handle_signal(int sig, siginfo_t *info, void *context)
+{
+	static char	c;
+	static int	count_bit;
+
+	(void)context;
+	if (info->si_pid != g_pid_c)
+	{
+		g_pid_c = info->si_pid;
+		c = 0;
+		count_bit = 0;
+	}
+	if (sig == SIGUSR2)
+		c = c | (1 << (7 - count_bit));
+	count_bit++;
+	if (count_bit == 8)
+	{
+		write(1, &c, 1);
+		if (c == '\0')
+			kill(info->si_pid, SIGUSR1);
+		c = 0;
+		count_bit = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	signal;
+
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+	g_pid_c = -1;
+	signal.sa_sigaction = handle_signal;
+	signal.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &signal, NULL) == -1 \
+		|| sigaction(SIGUSR2, &signal, NULL) == -1)
+		return (1);
+	while (1)
+		;
+	return (0);
+}
---
+++ minitalk/mendatory/client.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 00:08:40 by wkannouf          #+#    #+#             */
+/*   Updated: 2025/04/27 06:30:24 by wkannouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <limits.h>
+#include <signal.h>
+
+static short	overflow(size_t result, size_t b_result)
+{
+	if ((result / 10 != b_result) || (result >= LONG_MAX))
+		return (0);
+	return (1);
+}
+
+int	ft_atoi(const char *str)
+{
+	size_t	i;
+	int		result;
+	int		b_result;
+
+	i = 0;
+	result = 0;
+	if (!str)
+		return (0);
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (!(str[i] >= '0' && str[i] <= '9'))
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		b_result = result;
+		result = result * 10 + str[i++] - 48;
+		if (overflow(result, b_result) == 0)
+			return (0);
+	}
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i])
+		return (0);
+	return (result);
+}
+
+static void	send_bit(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if (((c >> (7 - bit)) & 1) == 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		bit++;
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	pid;
+
+	i = 0;
+	pid = ft_atoi(argv[1]);
+	if (argc != 3)
+		return (write(2, "argv != 3\n", 10), 1);
+	if (kill(pid, 0) == -1 || pid <= 0)
+		return (write(2, "Invalid PID.\n", 13), 1);
+	while (argv[2][i])
+	{
+		send_bit(pid, argv[2][i]);
+		i++;
+	}
+	send_bit(pid, '\n');
+	return (0);
+}
