a7de2a8dbdfdd24c2c81021eaef60f115844144a
Wkannouf <wkannouf@student.42.fr>
2025-06-01 19:22:03 +0100

    philo

--- philo/philo/philo.c
+++ philo/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: wkannouf <wkannouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 10:49:59 by wkannouf          #+#    #+#             */
-/*   Updated: 2025/05/26 11:05:05 by wkannouf         ###   ########.fr       */
+/*   Updated: 2025/06/01 19:15:54 by wkannouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ static int	arguments(int argc, char **argv, t_rules *rules)
 	rules->is_death = 0;
 	rules->count_philos = ft_atoi(argv[1]);
 	if (rules->count_philos == 0)
-		return (0);
+		return (5);
 	rules->time_to_die = ft_atoi(argv[2]);
 	rules->time_to_eat = ft_atoi(argv[3]);
 	rules->time_to_sleep = ft_atoi(argv[4]);
@@ -84,9 +84,9 @@ static int	one_philo(t_philo *philos)
 		printf("0 1 has taken a fork\n");
 		pthread_mutex_unlock(&philos->rules->forks[philos->left]);
 		time_mange(philos->rules->time_to_die, NULL);
-		return (printf("%zu 1 died\n", philos->rules->time_to_die), 1);
+		return (printf("%zu 1 died\n", philos->rules->time_to_die), 0);
 	}
-	return (0);
+	return (1);
 }
 
 int	main(int argc, char **argv)
